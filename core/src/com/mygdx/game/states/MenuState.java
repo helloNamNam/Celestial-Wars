1c7c0061f7cbf3bb15da55b2adac6b285436ece4
Tanawat Kusungnoen <olarmiezzlo@gmail.com>
2015-10-24 20:12:32 +0900

    Create MenuState (Don't use now, is can't run)

---
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -0,0 +1,33 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State{
+
+	public MenuState(GameStateManager gsm) {
+		super(gsm);
+	}
+
+	@Override
+	public void handleInput() {
+		
+	}
+
+	@Override
+	public void update(float dt) {
+		handleInput();
+		
+	}
+
+	@Override
+	public void render(SpriteBatch sb) {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+	
+
+}
